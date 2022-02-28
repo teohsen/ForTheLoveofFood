c22c02f1cbee17128c94a6774b76feb223da1ea1
Seth <teohsen@hotmail.com>
2022-02-28 20:16:59 +0800

    Minor Correction

--- tests/computer_vision.py
+++ tests/computer_vision.py
@@ -2,7 +2,7 @@ import hashlib
 from datetime import datetime
 
 from src.computer_vision.detect_food import *
-from src.computer_vision.detect_food import _CONFIG, _CUSTOM_WEIGHTS, _CUSTOM_CONFIG, _CUSTOM_CLASSES, _TESTOUT
+from src.computer_vision.detect_food import _CUSTOM_WEIGHTS, _CUSTOM_CONFIG, _CUSTOM_CLASSES, _TESTOUT
 
 shibasushi = YoloV4Custom(_CUSTOM_WEIGHTS.as_posix(), _CUSTOM_CONFIG.as_posix(), _CUSTOM_CLASSES)
 output_path = Path(_TESTOUT)
@@ -10,23 +10,20 @@ output_path = Path(_TESTOUT)
 
 
 def test_it(image_path, output=False):
-    image = cv2.imread(image_path)
-    classes, confi, bbox = shibasushi.predict(image)
+    image = cv2.imread(image_path.as_posix())
+    classes, confi, bbox = shibasushi.predict(image, nmsthreshold=0.1)
     output_image = shibasushi.draw(image, classes, confi, bbox)
-    cv2.imshow("image", image)
-    cv2.imshow("output_image", output_image)
+
+    cv2.imshow("image", cv2.resize(image, (600, 400)))
+    cv2.imshow("output_image", cv2.resize(output_image, (600, 400)))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     if output:
         tmp_name = hashlib.sha256(datetime.now().__str__().encode("utf-8")).hexdigest()
-        cv2.imwrite(f"{output_path.joinpath(tmp_name).with_suffix('.jpg')}", output_image)
-
-    return output_image, classes, confi, bbox
+        cv2.imwrite(f"{output_path.joinpath(tmp_name).with_suffix('.jpg').as_posix()}", output_image)
 
 
-test_list = [
-    r"test\photo_2021-10-25_18-39-01.jpg"
-]
-
-processed_image, predictions = test_it(test_list[0])
+filepath = Path("data/test").glob("*")
+for file in filepath:
+    test_it(file, output=True)
